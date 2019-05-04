4c45152eef16634ab47d7c56462cfcfd8ad6936b
Welder Mileu <welderfelix21@gmail.com>
2019-05-04 01:11:36 -0300

    Em desenvolvimento

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// setTimeout(() => {
+// 	alert("Seja Bem vindo site em Obras")
+// },5000)
+
 // Horario
 
 // Hora
@@ -24,6 +28,73 @@ const calendario = document.getElementById("ano");
 
 calendario.innerHTML = `${horario}`;
 
-// setTimeout(() => {
-// 	alert("Seja Bem vindo site em Obras")
-// },5000)
+let conteudo = document.getElementById("conteudo");
+
+const content =[
+	{
+		title:"Destaque do Esporte",
+		text:"Confira os melhores momentos do jogo entre Cruzeiro e Flamengo na integra.",
+		subtext:`Lorem ipsum dolor sit amet, 
+					consectetur adipisicing elit, 
+					sed do eiusmod tempor incididunt ut 
+					labore et dolore magna aliqua. Ut enim ad minim veniam, 
+					quis nostrud exercitation ullamco laboris nisi ut aliquip 
+					ex ea commodo consequat.`,
+		img:"img/cruzeiro.jpg"
+	},
+	{
+		title:"Destaque da Politica",
+		text:"Presidente Bolsonaro libera K47 e Fuzil para a populaçao confira mais na integra.",
+		subtext:`Lorem ipsum dolor sit amet, 
+					consectetur adipisicing elit, 
+					sed do eiusmod tempor incididunt ut 
+					labore et dolore magna aliqua. Ut enim ad minim veniam, 
+					quis nostrud exercitation ullamco laboris nisi ut aliquip 
+					ex ea commodo consequat.`,
+		img:"img/bolsonaro.jpg"
+	},
+	{
+		title:"Destaque da Tecnologia",
+		text:"Foi confirmado as melhores areas para se atuar na tecnologia confira mais na integra.",
+		subtext:`Lorem ipsum dolor sit amet, 
+					consectetur adipisicing elit, 
+					sed do eiusmod tempor incididunt ut 
+					labore et dolore magna aliqua. Ut enim ad minim veniam, 
+					quis nostrud exercitation ullamco laboris nisi ut aliquip 
+					ex ea commodo consequat.`,
+		img:"img/tecnologia.jpg"
+	},
+
+];
+
+const exibir = content.map(( form )=>{
+	return conteudo.innerHTML += `<div class="container mt-3 mb-3">
+			<div class="card mt-3">
+				<div class="card-body">
+					<div class="float-left">
+						<div>
+							<img src="${form.img}" width="100" class="image-top">
+						</div>
+						<div>
+							<div class="card-title mb-3">
+								<p class="text-center text-muted">${form.title}</p>
+							</div>
+							<div>
+								<div class="card-text" style="text-align: center">
+									<p>${form.text}</p>
+								</div>
+								<div class="card-description">
+									<p>${form.subtext}</p>
+								</div>
+								<div class="card-footer">
+									<button class="btn btn-integra">
+										Confira na Integra
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>`;
+})
